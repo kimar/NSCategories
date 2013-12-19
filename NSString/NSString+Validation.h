f5444ca8d9b52656af1c4a3902037bcafe4ac288
Marcus Kida <marcus@kida.io>
2013-12-19 13:48:20 +0100

    Add NSString+Validation providing email validation

---
+++ NSString/NSString+Validation.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Validation.h
+//  
+//
+//  Created by Marcus Kida on 19.12.13.
+//
+//
+
+
+
+@interface NSString (Validation)
+
+- (BOOL)isValidEmail;
+
+@end
---
+++ NSString/NSString+Validation.m
@@ -0,0 +1,27 @@
+//
+//  NSString+Validation.m
+//  
+//
+//  Created by Marcus Kida on 19.12.13.
+//
+//
+
+#import "NSString+Validation.h"
+
+@implementation NSString (Validation)
+
+- (BOOL)isValidEmail;
+{
+    NSString *regex =
+    @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
+    @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
+    @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
+    @"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
+    @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
+    @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
+    @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    NSPredicate *test = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [test evaluateWithObject:[self lowercaseString]];
+}
+
+@end
